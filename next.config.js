9e4ab50305f1a5ce423e1474ba6154112bed155b
diptanshumahish <diptanshumahish2016@gmail.com>
2022-11-07 09:02:25 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable: process.env.NODE_ENV === 'development',
+  // buildExcludes: [/middleware-manifest.json$/],
+});
 
-module.exports = nextConfig
+const nextConfig = withPWA({
+  // next config
+});
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../src/config.js';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
